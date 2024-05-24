79854197bc8ed2eb7733f05d9b2af091bae947da
hemal1020 <85827167+hemal1020@users.noreply.github.com>
2024-05-24 12:31:49 +0600

    Add files via upload

---
+++ finalexam/vueproject/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue.js!</h1>
+    <p>This is my Vue.js project.</p>
+    <p>My name is Hemal Ahmed</p>
+    <p>I am from CSE20</p>
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  
+}
+</script>
+
+<style>
+  p{
+    font-size: 20px;
+    color: purple;
+  }
+</style>
